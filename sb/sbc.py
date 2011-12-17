8bcd8f73243d7ea9ce324626da6fc7d4efeceb3b
tuxpenguin <sidtux@gmail.com>
2011-12-17 21:16:11 +0100

    Adapted sb branch in order to be packaged.

--- sb/sbc.py
+++ sb/sbc.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python
 # -*- coding=utf-8 -*-
 # Semplice Backup Console
+
+####
+#
 # Copyright © 2011 Semplice Team. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author(s): Luca "sidtux" B. <sidtux@gmail.com>
+#
+####
 
 import os, sys, locale, gettext, libcompress, libftp
 
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+# Semplice Backup setup file
+# Copyright © 2011 Semplice Team. All rights reserved.
+
+import os
+from distutils.core import setup
+
+setup(name = "sb",
+      version = "0.1",
+      author = 'Luca "sidtux" B.',
+      author_email = "sidtux@gmail.com",
+      description = "A simple backup program.",
+      url = "http://semplice-linux.sourceforge.net",
+      packages=['sb'],
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+      ],
+      requires=['os', 'sys', 'gi', 'sqlite3', 'tarfile', 'zipfile', 'time', 'ftplib', 'gettext']
+)
